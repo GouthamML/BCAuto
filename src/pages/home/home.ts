8ea498beca4abc25fe6c69b89f745e0e668ce053
gml <gml@GML-IN.in.oracle.com>
2018-10-30 20:03:18 +0530

    Adding and reading from BC

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,7 @@ import { NavController } from 'ionic-angular';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { Toast } from '@ionic-native/toast';
 import { AlertController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
 
 import 'rxjs/add/operator/map'
 
@@ -35,9 +36,59 @@ export class HomePage {
 
 
   constructor(public navCtrl: NavController, private alertCtrl: AlertController, private barcodeScanner: BarcodeScanner,
-    private toast: Toast, private httpclient:HttpClient) {
+    private toast: Toast, private httpclient:HttpClient, public loadingCtrl: LoadingController) {
     //  this.callFunc();
   }
+  /*
+  adding to blockchain
+  */
+ openAlertAddOBC(){
+  let url = "https://2E42370EAFB342A99759C7B7378C46D0.blockchain.ocp.oraclecloud.com:443/restproxy1/bcsgw/rest/v1/transaction/invocation";
+  let barcode = JSON.parse(localStorage.getItem('barCode'));
+  
+  let body = JSON.parse(localStorage.getItem('body'));
+  console.log("barcode###########\n" + JSON.stringify(barcode) );
+  
+  if(barcode["chassisNumber"] == null){
+    body["method"] = "initVehiclePart";
+
+  }
+  else{
+    body["method"] = "initVehicle";
+  }
+  console.log("barcode type" + Object.keys(barcode));
+
+  for(let i=0; i < Object.keys(barcode).length; i++){
+    console.log("barcode data in loop\n" + barcode[Object.keys(barcode)[i]]);
+    body["args"][i] = barcode[Object.keys(barcode)[i]];
+  }
+
+  console.log("JSONbody for adding to blockchain#####\n" + JSON.stringify(body));
+
+
+  this.httpclient.post(url, body, httpOptions)
+  .subscribe(response => {
+    console.log("response for adding blockchain###\n" +  JSON.stringify(response));
+    if(response['returnCode'] == "Success"){
+      let alert = this.alertCtrl.create({
+        title: 'Added Successfully',
+        subTitle: 'Scanned assets have been added to the Oracle block Chain',
+        buttons: ['Done']
+      });
+      alert.present();
+    }
+    else{
+      let alert = this.alertCtrl.create({
+        title: 'Couldn\'t Add',
+        subTitle: 'Please try again',
+        buttons: ['Done']
+      });
+      alert.present();
+
+    }
+  })
+
+}
 
   openAlertSuccess() {
     let alert = this.alertCtrl.create({
@@ -80,7 +131,22 @@ export class HomePage {
           text: 'Yes',
           handler: () => {
             console.log('Buy clicked');
-            this.navCtrl.push(InfoPage);
+
+            this.openAlertAddOBC();
+            /*
+            Opening loading screen!!
+             */
+              let loading = this.loadingCtrl.create({
+                content: 'Please wait...'
+              });
+            
+              loading.present();
+            
+              setTimeout(() => {
+                loading.dismiss();
+              }, 5000);
+
+            
             
           }
         }
@@ -89,6 +155,8 @@ export class HomePage {
     alert.present();
 
   }
+
+  
 
 
    scan() {
@@ -111,21 +179,33 @@ export class HomePage {
 
       if (this.jsonOfBarcode.chassisNumber == null){
         jsonBody["method"] = "readVehiclePart";
+        jsonBody.args.push(this.jsonOfBarcode.SerialNumber);
       }
       else{
-        jsonBody["method"] = "initVehiclePart";
+        jsonBody["method"] = "readVehicle";
+        jsonBody.args.push(this.jsonOfBarcode.chassisNumber);
       }
-      for(let i in this.jsonOfBarcode) {
-        jsonBody.args.push(this.jsonOfBarcode[i]);
-      } 
+      
+      
+      // for(let i in this.jsonOfBarcode) {
+      //   jsonBody.args.push(this.jsonOfBarcode[i]);
+      // } 
       jsonBody["chaincodeVer"] = "v1";
-      console.log(jsonBody);
+      console.log("This is jsonbody" + JSON.stringify(jsonBody));
+      console.log("This is barcode data" + JSON.stringify(this.jsonOfBarcode));
       this.test = this.jsonOfBarcode;
+
+      /*
+      POST API call
+       */
       
       this.httpclient.post(url,jsonBody,httpOptions)
       .subscribe(response => {
         localStorage.setItem('res', JSON.stringify(response));
+        localStorage.setItem('body', JSON.stringify(jsonBody));
+        localStorage.setItem('barCode', JSON.stringify(this.jsonOfBarcode));  //response is from blockcahin
         if( response['returnCode'] == "Success" ){
+          console.log("response for success" + response);
           this.openAlertSuccess();
         }
         else{
@@ -134,8 +214,7 @@ export class HomePage {
         
       });
     })
-    localStorage.setItem('body', JSON.stringify(this.jsonBody));
-    localStorage.setItem('barCode', JSON.stringify(this.jsonOfBarcode)); 
+    
   
   }
 
