29492c98fbbe084edc9d3c0cd245e1ff4df7b05c
gml <gml@GML-IN.in.oracle.com>
2018-11-08 19:57:04 +0530

    Transfer with validation

--- src/pages/info/info.ts
+++ src/pages/info/info.ts
@@ -36,27 +36,51 @@ export class InfoPage {
   Values:any;
   jsonbody:any;
   argsTemp:any;
+  credentials:any;
+  barCodeData:any;
+  show:boolean = false;
   constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController,  private httpclient:HttpClient) {
     this.response = JSON.parse(localStorage.getItem('res'));
     this.response = JSON.parse(this.response['result']['payload']);
     console.log(this.response);
     this.test = JSON.stringify(this.response);
     let i;
-    this.Keys = Object.keys(this.response);    
+    this.Keys = Object.keys(this.response);
+
+    this.barCodeData = JSON.parse(localStorage.getItem('barCode'));
+    this.credentials = JSON.parse(localStorage.getItem('credentials'));
+    this.jsonbody = JSON.parse(localStorage.getItem('body'));
+
+   
+    this.argsTemp = this.jsonbody.args;
+
+    //checking for vehicle or part
+
+    if ( this.barCodeData.chassisNumber == null && this.credentials.username == "supplier"){
+    this.jsonbody.method = "transferVehiclePart";
+    this.show = true;
+    }
+    else if(this.barCodeData.SerialNumber == null && (this.credentials.username=="manufacturer"||this.credentials.username=="dealer"))
+    {
+      this.jsonbody.method = "transferVehicle"
+      this.show = true;
+  
+    }
+    else {
+      //show will be false
+
+    }
+
+    ///////////////////////////////////////    
   }
   
   ionViewDidLoad() {
     console.log('ionViewDidLoad InfoPage');
   }
   transfer(){
-    this.jsonbody = JSON.parse(localStorage.getItem('body'));
-    this.argsTemp = this.jsonbody.args;
-    if ( this.jsonbody.chassisNumber == null){
-    this.jsonbody.method = "transferVehiclePart"
-    }
-    else {
-      this.jsonbody.method = "transferVehicle"
-    }
+    
+
+
     this.jsonbody.args.push(this.response["owner"]);
     let alert = this.alertCtrl.create({
       title: 'Transfer',
@@ -80,13 +104,14 @@ export class InfoPage {
           handler: data => {
             this.jsonbody.args.push(data.ownername);
             console.log("This is BODY for transfer" + JSON.stringify(this.jsonbody));
+            
             //making post request for transfer vehicle /part
             this.httpclient.post(url, this.jsonbody, httpOptions)
             .subscribe(postResponse => {
               if( postResponse['returnCode'] == "Success"){
                 let alert = this.alertCtrl.create({
                   title: 'Transfered Successfully',
-                  subTitle: 'Ownership has been changed from'+this.response["owner"]+' to' + data.ownername,
+                  subTitle: 'Ownership has been changed from'+this.response["owner"]+' to ' + data.ownername,
                   buttons: ['Dismiss']
                 });
                 alert.present();
